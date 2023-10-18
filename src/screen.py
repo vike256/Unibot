6c345c9f250ddd93676c5fea06444e860b56f2a2
vike256 <niila.viinamaki@gmail.com>
2023-10-18 20:52:54 +0300

    cleaning code

--- src/screen.py
+++ src/screen.py
@@ -25,48 +25,6 @@ def get_target():
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(hsv, cfg.lower_color, cfg.upper_color)
 
-        if cfg.aim_type == 'pixel':
-            for y in range(cfg.fov):
-                for x in range(cfg.fov):
-                    pixel = hsv[y, x]
-                    h, s, v = pixel
-                    img_rel_x = x - cfg.center[0]
-                    img_rel_y = y - cfg.center[1]
-                    distance = np.sqrt(img_rel_x**2 + img_rel_y**2)
-                    if (distance <= cfg.fov // 2
-                    and cfg.lower_color[0] <= h and h <= cfg.upper_color[0]
-                    and cfg.lower_color[1] <= s and s <= cfg.upper_color[1]
-                    and cfg.lower_color[2] <= v and v <= cfg.upper_color[2]):
-                        if distance < min_distance:
-                            min_distance = distance
-                            target = (img_rel_x + cfg.center[0], img_rel_y + cfg.center[1])
-            if min_distance == 0:
-                trigger = True
-
-        elif cfg.aim_type == 'shape':
-            kernel = np.ones((3,3), np.uint8)
-            dilated = cv2.dilate(mask, kernel, iterations=5)
-            thresh = cv2.threshold(dilated, 60, 255, cv2.THRESH_BINARY)[1]
-            contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-            if len(contours) != 0:
-                for contour in contours:
-                    x, y, w, h = cv2.boundingRect(contour)
-                    cX = x + w // 2
-                    cY = y + h // cfg.head_height
-                    distance = np.sqrt((cX - cfg.center[0])**2 + (cY - cfg.center[1])**2)
-                    if distance < min_distance:
-                        min_distance = distance
-                        closest_contour = contour
-                        target = (cX, cY)
-            
-            value = 8
-            if thresh[cfg.center[0] + value, cfg.center[1]] == 255:
-                if thresh[cfg.center[0] - value, cfg.center[1]] == 255:
-                    if thresh[cfg.center[0], cfg.center[1] - value] == 255:
-                        if thresh[cfg.center[0], cfg.center[1] + value] == 255:
-                            trigger = True
-
         if cfg.debug:
             if cfg.aim_type == 'pixel':
                 # Draw line to closest target
@@ -101,5 +59,48 @@ def get_target():
             img = cv2.resize(img, window_resolution)
             cv2.imshow(window_name, img)
             cv2.waitKey(1)
+
+        if cfg.aim_type == 'pixel':
+            for y in range(cfg.fov):
+                for x in range(cfg.fov):
+                    pixel = hsv[y, x]
+                    h, s, v = pixel
+                    img_rel_x = x - cfg.center[0]
+                    img_rel_y = y - cfg.center[1]
+                    distance = np.sqrt(img_rel_x**2 + img_rel_y**2)
+                    if  distance <= cfg.fov // 2 and \
+                        cfg.lower_color[0] <= h and h <= cfg.upper_color[0] and \
+                        cfg.lower_color[1] <= s and s <= cfg.upper_color[1] and \
+                        cfg.lower_color[2] <= v and v <= cfg.upper_color[2]:
+                        if distance < min_distance:
+                            min_distance = distance
+                            target = (img_rel_x + cfg.center[0], img_rel_y + cfg.center[1])
+                            if distance == 0:
+                                trigger = True
+                                return target, trigger
+
+        elif cfg.aim_type == 'shape':
+            kernel = np.ones((3,3), np.uint8)
+            dilated = cv2.dilate(mask, kernel, iterations=5)
+            thresh = cv2.threshold(dilated, 60, 255, cv2.THRESH_BINARY)[1]
+            contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+            if len(contours) != 0:
+                for contour in contours:
+                    x, y, w, h = cv2.boundingRect(contour)
+                    cX = x + w // 2
+                    cY = y + h // cfg.head_height
+                    distance = np.sqrt((cX - cfg.center[0])**2 + (cY - cfg.center[1])**2)
+                    if distance < min_distance:
+                        min_distance = distance
+                        closest_contour = contour
+                        target = (cX, cY)
+            
+            value = 8
+            if  thresh[cfg.center[0] + value, cfg.center[1]] == 255 and \
+                thresh[cfg.center[0] - value, cfg.center[1]] == 255 and \
+                thresh[cfg.center[0], cfg.center[1] - value] == 255 and \
+                thresh[cfg.center[0], cfg.center[1] + value] == 255:
+                trigger = True
     
     return target, trigger

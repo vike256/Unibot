876fb6414fc95e88cee5a11af205edb7176a4b9f
vike <69812383+vike256@users.noreply.github.com>
2025-07-23 16:00:52 +0300

    Move code from main to separate file unibot.py

--- src/main.py
+++ src/main.py
@@ -18,80 +18,12 @@
 import time
 import numpy as np
 
-from cheats import Cheats
-from mouse import Mouse
-from screen import Screen
-from utils import Utils
+from unibot import Unibot
 
 
 def main():
-    # Print licensing info
-    print('''
-Unibot  Copyright (C) 2025  vike256
-This program comes with ABSOLUTELY NO WARRANTY.
-This is free software, and you are welcome to redistribute it under certain conditions.
-For details see <LICENSE.txt>.
-    ''')
-
-    # Program loop
-    while True:
-        # Track delta time
-        start_time = time.time()
-
-        utils = Utils()
-        config = utils.config
-        cheats = Cheats(config)
-        mouse = Mouse(config)
-        screen = Screen(config)
-
-        print('Unibot ON')
-
-        # Cheat loop
-        while True:
-            delta_time = time.time() - start_time
-            start_time = time.time()
-            
-            reload_config = utils.check_key_binds()
-            if reload_config:
-                break
-
-            if (utils.get_aim_state() or utils.get_trigger_state()) or (config.debug and config.debug_always_on):
-                # Get target position and check if there is a target in the center of the screen
-                target, trigger = screen.get_target(cheats.recoil_offset)
-
-                # Shoot if target in the center of the screen
-                if utils.get_trigger_state() and trigger:
-                    if config.trigger_delay != 0:
-                        delay_before_click = (np.random.randint(config.trigger_randomization) + config.trigger_delay) / 1000
-                    else:
-                        delay_before_click = 0
-                    mouse.click(delay_before_click)
-
-                # Calculate movement based on target position
-                cheats.calculate_aim(utils.get_aim_state(), target)
-
-            if utils.get_rapid_fire_state():
-                mouse.click()
-
-            # Apply recoil
-            cheats.apply_recoil(utils.recoil_state, delta_time)
-
-            # Move the mouse based on the previous calculations
-            mouse.move(cheats.move_x, cheats.move_y)
-
-            # Reset move values so the aim doesn't keep drifting when no targets are on the screen
-            cheats.move_x, cheats.move_y = (0, 0)
-
-            # Do not loop above the set refresh rate
-            time_spent = (time.time() - start_time) * 1000
-            if time_spent < screen.min_loop_time:
-                time.sleep((screen.min_loop_time - time_spent) / 1000)
-
-        del utils
-        del cheats
-        del mouse
-        del screen
-        print('Reloading')
+    unibot = Unibot()
+    unibot.run()
 
 
 if __name__ == "__main__":
